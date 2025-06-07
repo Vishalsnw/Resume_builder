59196fd87b84f02b3ed6346d96325d82bd001b71
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-08 01:45:35 +0530

    Create _document.tsx

---
+++ client/src/pages/_document.tsx
@@ -0,0 +1,122 @@
+// pages/_document.tsx
+
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Character Set */}
+          <meta charSet="utf-8" />
+
+          {/* Favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2d89ef" />
+          <meta name="theme-color" content="#ffffff" />
+
+          {/* Fonts */}
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+
+          {/* PWA meta tags */}
+          <meta name="application-name" content="Resume Builder" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Resume Builder" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+
+          {/* Microsoft Tile */}
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="msapplication-tap-highlight" content="no" />
+
+          {/* Security headers */}
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.example.com;" />
+          <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
+          <meta httpEquiv="Referrer-Policy" content="strict-origin-when-cross-origin" />
+
+          {/* Preload critical assets */}
+          <link
+            rel="preload"
+            href="/fonts/custom-font.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
+          {/* Structured Data */}
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "http://schema.org",
+                "@type": "WebApplication",
+                "name": "Resume Builder",
+                "description": "Create professional resumes easily with our resume builder.",
+                "applicationCategory": "BusinessApplication",
+                "operatingSystem": "Web",
+                "offers": {
+                  "@type": "Offer",
+                  "price": "0",
+                  "priceCurrency": "USD"
+                }
+              })
+            }}
+          />
+        </Head>
+        <body>
+          {/* Preloader */}
+          <noscript>
+            <div className="noscript-message">
+              You need to enable JavaScript to run this app.
+            </div>
+          </noscript>
+
+          {/* Application Mount Point */}
+          <Main />
+
+          {/* Next.js Scripts */}
+          <NextScript />
+
+          {/* Version Info Comment */}
+          {/* Version: 1.0.0 */}
+          {/* Last updated by Vishalsnw at 2025-06-07 20:14:20 */}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
