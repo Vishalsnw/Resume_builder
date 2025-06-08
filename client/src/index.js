53e3aff05408d83ba1c6ff543fcf54455ee9d136
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-08 22:15:50 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app'; // Adjust the path if App.tsx is in a different location
-import './index.css'; // Optional, include if you have a global CSS file
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root') // Ensure this matches the `id` in your public/index.html
+  </React.StrictMode>
 );
