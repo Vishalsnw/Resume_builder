d5979f232305d04723ee78799d57d6162a3027e1
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-09 02:30:01 +0530

    Update react-icons.d.ts

--- client/src/react-icons.d.ts
+++ client/src/react-icons.d.ts
@@ -11,6 +11,7 @@ declare module 'react-icons/fi' {
   
   export type IconType = ComponentType<IconBaseProps>;
   
+  // Icons from previous file
   export const FiLoader: IconType;
   export const FiCheck: IconType;
   export const FiX: IconType;
@@ -18,4 +19,38 @@ declare module 'react-icons/fi' {
   export const FiChevronRight: IconType;
   export const FiLock: IconType;
   export const FiHelpCircle: IconType;
+  
+  // New icons from error messages
+  export const FiExternalLink: IconType;
+  export const FiLink2: IconType;
+  export const FiInfo: IconType;
+  export const FiAlertTriangle: IconType;
+  export const FiChevronDown: IconType;
+  
+  // Additional common Feather icons that might be used
+  export const FiUser: IconType;
+  export const FiSettings: IconType;
+  export const FiEdit: IconType;
+  export const FiTrash: IconType;
+  export const FiPlus: IconType;
+  export const FiMinus: IconType;
+  export const FiSearch: IconType;
+  export const FiEye: IconType;
+  export const FiEyeOff: IconType;
+  export const FiCalendar: IconType;
+  export const FiClock: IconType;
+  export const FiDownload: IconType;
+  export const FiUpload: IconType;
+  export const FiLogOut: IconType;
+  export const FiLogIn: IconType;
+  export const FiMenu: IconType;
+  export const FiHome: IconType;
+  export const FiSave: IconType;
+  export const FiArrowLeft: IconType;
+  export const FiArrowRight: IconType;
+  export const FiArrowUp: IconType;
+  export const FiArrowDown: IconType;
+  export const FiMail: IconType;
+  export const FiPhone: IconType;
+  export const FiStar: IconType;
 }
