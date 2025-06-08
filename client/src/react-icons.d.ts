881e4585e86ed300bd81171a753e8a77b19b910f
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-09 02:41:57 +0530

    Update react-icons.d.ts

--- client/src/react-icons.d.ts
+++ client/src/react-icons.d.ts
@@ -20,16 +20,15 @@ declare module 'react-icons/fi' {
   export const FiLock: IconType;
   export const FiHelpCircle: IconType;
   
-  // New icons from previous errors
+  // Icons from errors
   export const FiExternalLink: IconType;
   export const FiLink2: IconType;
   export const FiInfo: IconType;
   export const FiAlertTriangle: IconType;
   export const FiChevronDown: IconType;
-  
-  // New icons from the latest error
   export const FiMaximize2: IconType;
   export const FiMinimize2: IconType;
+  export const FiCheckCircle: IconType; // Added the missing icon
   
   // Additional common Feather icons that might be used
   export const FiUser: IconType;
@@ -67,4 +66,66 @@ declare module 'react-icons/fi' {
   export const FiTwitter: IconType;
   export const FiDollarSign: IconType;
   export const FiFileText: IconType;
-    }
+  
+  // All other Feather icons
+  export const FiActivity: IconType;
+  export const FiAirplay: IconType;
+  export const FiAlertOctagon: IconType;
+  export const FiAnchor: IconType;
+  export const FiAperture: IconType;
+  export const FiArchive: IconType;
+  export const FiArrowDownCircle: IconType;
+  export const FiArrowDownLeft: IconType;
+  export const FiArrowDownRight: IconType;
+  export const FiArrowLeftCircle: IconType;
+  export const FiArrowRightCircle: IconType;
+  export const FiArrowUpCircle: IconType;
+  export const FiArrowUpLeft: IconType;
+  export const FiArrowUpRight: IconType;
+  export const FiBell: IconType;
+  export const FiBellOff: IconType;
+  export const FiBluetooth: IconType;
+  export const FiBold: IconType;
+  export const FiBook: IconType;
+  export const FiBookmark: IconType;
+  export const FiBox: IconType;
+  export const FiBriefcase: IconType;
+  export const FiCamera: IconType;
+  export const FiCameraOff: IconType;
+  export const FiChevronsDown: IconType;
+  export const FiChevronsLeft: IconType;
+  export const FiChevronsRight: IconType;
+  export const FiChevronsUp: IconType;
+  export const FiCloud: IconType;
+  export const FiCloudDrizzle: IconType;
+  export const FiCloudLightning: IconType;
+  export const FiCloudOff: IconType;
+  export const FiCloudRain: IconType;
+  export const FiCloudSnow: IconType;
+  export const FiCode: IconType;
+  export const FiCodepen: IconType;
+  export const FiCodesandbox: IconType;
+  export const FiCoffee: IconType;
+  export const FiColumns: IconType;
+  export const FiCommand: IconType;
+  export const FiCompass: IconType;
+  export const FiCopy: IconType;
+  export const FiCornerDownLeft: IconType;
+  export const FiCornerDownRight: IconType;
+  export const FiCornerLeftDown: IconType;
+  export const FiCornerLeftUp: IconType;
+  export const FiCornerRightDown: IconType;
+  export const FiCornerRightUp: IconType;
+  export const FiCornerUpLeft: IconType;
+  export const FiCornerUpRight: IconType;
+  export const FiCpu: IconType;
+  export const FiCreditCard: IconType;
+  export const FiCrop: IconType;
+  export const FiCrosshair: IconType;
+  export const FiDatabase: IconType;
+  export const FiDelete: IconType;
+  export const FiDisc: IconType;
+  export const FiDivide: IconType;
+  export const FiDivideCircle: IconType;
+  export const FiDivideSquare: IconType;
+}
