f3705d514078afa97d76a42b5db5ef73789b972f
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-08 20:59:05 +0530

    Create app.constants.ts

---
+++ shared/constants/app.constants.ts
@@ -0,0 +1,7 @@
+export const AppConstants = {
+  DEFAULT_CURRENCY: 'USD',
+  OTP_EXPIRATION_TIME: 10 * 60 * 1000, // 10 minutes in milliseconds
+  DEFAULT_PAGE_SIZE: 10,
+  SUPPORTED_FILE_TYPES: ['application/pdf', 'application/msword'],
+  STRIPE_CURRENCY: 'usd',
+};
