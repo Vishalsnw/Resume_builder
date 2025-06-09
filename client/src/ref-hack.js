f8792a22686fe553402f678eed79a58e72dae25d
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-09 13:34:56 +0530

    Create ref-hack.js

---
+++ client/src/ref-hack.js
@@ -0,0 +1,60 @@
+// src/ref-hack.js
+
+// Monkeypatch React.RefObject to make current writable
+// This is a hack and should be used with caution!
+if (typeof window !== 'undefined') {
+  const originalCreateRef = React.createRef;
+  
+  React.createRef = function() {
+    const ref = originalCreateRef();
+    
+    // Make the current property writable
+    Object.defineProperty(ref, 'current', {
+      get: function() { return this._current; },
+      set: function(value) { this._current = value; },
+      enumerable: true,
+      configurable: true
+    });
+    
+    return ref;
+  };
+
+  // Fix useRef too
+  const originalUseRef = React.useRef;
+  
+  React.useRef = function(initialValue) {
+    const ref = originalUseRef(initialValue);
+    
+    // Make the current property writable
+    if (!ref.hasOwnProperty('_current')) {
+      Object.defineProperty(ref, '_current', {
+        value: initialValue,
+        writable: true,
+        enumerable: false
+      });
+      
+      Object.defineProperty(ref, 'current', {
+        get: function() { return this._current; },
+        set: function(value) { this._current = value; },
+        enumerable: true,
+        configurable: true
+      });
+    }
+    
+    return ref;
+  };
+}
+
+// Add a global variable to the window that can be used to bypass the readonly restriction
+// Usage: window.__assignRef(myRef, someElement);
+if (typeof window !== 'undefined') {
+  window.__assignRef = (ref, value) => {
+    if (ref) {
+      Object.defineProperty(ref, 'current', {
+        value,
+        writable: true,
+        configurable: true
+      });
+    }
+  };
+}
